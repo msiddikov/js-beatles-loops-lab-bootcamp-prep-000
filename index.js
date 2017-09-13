a8fd16415de1f2a4d5febeee2b3c52020e2b0b9f
Mukhammadayub Siddikov <m.siddikov@gmail.com>
2017-09-13 09:49:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(artist, intrument){
   var str = [];
-  for (let i=0; i < artist.length or i<instrument.length;i++){
+  for (let i=0; i < artist.length || i<instrument.length;i++){
     str.push(`${artist[i]} plays ${instrument[i]}`);
   }
 
